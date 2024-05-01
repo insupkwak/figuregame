d0502d65a322c33fb1147d101b7d4df5365899d4
InsupKwak <152236616+insupkwak@users.noreply.github.com>
2024-05-01 18:28:47 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ data_file = 'attempts.json'
 def load_data():
     try:
         with open(data_file, 'r') as f:
-            return json.load(f)
+            data = json.load(f)
+            # 모든 데이터의 'digits' 필드를 문자열로 통일
+            for attempt in data:
+                attempt['digits'] = str(attempt['digits'])
+            return data
     except (FileNotFoundError, json.JSONDecodeError):
         return []
+    
 
 def save_data(data):
     with open(data_file, 'w') as f:
@@ -32,6 +37,7 @@ def home():
 @app.route('/attempts', methods=['POST'])
 def record_attempt():
     data = request.get_json()
+
     if not data or not all(key in data for key in ['digits', 'attempts', 'username']):
         return jsonify({'error': 'Missing data'}), 400
 
@@ -42,6 +48,7 @@ def record_attempt():
    
     attempts_log.append(data)
 
+
     save_data(attempts_log)  # 데이터를 파일에 저장
 
     return jsonify({'message': 'Data saved successfully', 'data': data}), 201
@@ -49,16 +56,10 @@ def record_attempt():
 
 @app.route('/attempts/<int:digits>', methods=['GET'])
 def get_attempts(digits):
-
     digits_str = str(digits)
     attempts_log = load_data()  # 파일에서 데이터를 로드
-    
-
     filtered_attempts = [attempt for attempt in attempts_log if attempt['digits'] == digits_str]
     sorted_attempts = sorted(filtered_attempts, key=lambda x: x['attempts'])
-    
-    print(sorted_attempts)
-
     return jsonify(sorted_attempts)
 
 
